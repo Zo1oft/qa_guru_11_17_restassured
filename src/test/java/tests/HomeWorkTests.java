aaa99d059a82aa421fc0c6458ece111284df5cdb
Max Zhurkin <tryzoloft@gmail.com>
2022-04-11 16:06:20 +0500

    fixed names of methods added baseURl in @BeforeAll Added assertion in ListUsers Removed token name added nonNull assertion in loginSuccessful aswell.

--- src/test/java/tests/HomeWorkTests.java
+++ src/test/java/tests/HomeWorkTests.java
@@ -1,18 +1,34 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.http.ContentType.JSON;
-import static org.hamcrest.Matchers.is;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.*;
 
 public class HomeWorkTests {
 
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "https://reqres.in/";
+    }
+
     @Test
-    void SingleUserNotFound() {
+    void singleUserNotFound() {
         String response =
-                get("https://reqres.in/api/users/23")
+                get("/api/users/23")
                         .then()
                         .statusCode(404)
                         .extract().response().asString();
@@ -21,61 +37,58 @@ public class HomeWorkTests {
     }
 
     @Test
-    void PostCreate() {
+    void postCreate() {
         String data = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
-                given()
-                        .contentType(JSON)
-                        .body(data)
-                        .when()
-                        .post("https://reqres.in/api/users")
-                        .then()
-                        .statusCode(201)
-                        .body("name", is("morpheus"))
-                        .body("job", is("leader"));
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("/api/users")
+                .then()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
     }
 
     @Test
-    void LoginSuccessful() {
+    void loginSuccessful() {
         String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
         given()
                 .contentType(JSON)
                 .body(data)
                 .when()
-                .post("https://reqres.in/api/login")
+                .post("/api/login")
                 .then()
                 .statusCode(200)
-                .body("token", is("QpwL5tke4Pnpja7X4"));
+                .body("token", is(notNullValue()));
     }
 
     @Test
-    void ListUsers() {
+    void listUsers() {
+        get("/api/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("data", is(notNullValue()));
+
+    }
+
+    @Test
+    void deleteTest() {
         String response =
-                get("https://reqres.in/api/users?page=2")
+                delete("/api/users/2")
+                        .then()
+                        .statusCode(204)
+                        .extract().response().asString();
+        System.out.println(response);
+    }
+
+    @Test
+    void delayedTest() {
+        String response =
+                get("/users?delay=3")
                         .then()
                         .statusCode(200)
                         .extract().response().asString();
         System.out.println(response);
-
     }
-
-    @Test
-    void DeleteTest() {
-        String response =
-        delete("https://reqres.in/api/users/2")
-                .then()
-                .statusCode(204)
-                .extract().response().asString();
-        System.out.println(response);
-    }
-
-    @Test
-    void DelayedTest() {
-        String response =
-        get("https://reqres.in/api/users?delay=3")
-                .then()
-                .statusCode(200)
-                .extract().response().asString();
-        System.out.println(response);
-    }
-
 }
